d1fff1d1fa30688bac1b0f33882f3c96c9834048
bita <bitaaghanbarii@gmail.com>
2023-10-04 21:27:56 +0330

    verification code added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Landing from "./screens/Landing/Landing";
 import Login from "./screens/Authorize/Login";
 import Register from "./screens/Authorize/Register";
 import ForgetPassword from "./screens/Authorize/ForgetPassword";
+import VerificationCode from "./screens/Authorize/VerificationCode";
 const App = () => {
   const Router = createBrowserRouter([
     { path: "/", element: <Landing /> },
@@ -14,6 +15,7 @@ const App = () => {
         { path: "/authorize/login", element: <Login /> },
         { path: "/authorize/register", element: <Register /> },
         { path: "/authorize/forget", element: <ForgetPassword /> },
+        { path: "/authorize/verification", element: <VerificationCode/> },
       ],
     },
   ]);
--- src/screens/Authorize/VerificationCode.jsx
+++ src/screens/Authorize/VerificationCode.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 import { Formik, Form } from "formik";
 import * as yup from "yup";
-import pic3 from "../../assets/image/picture3.svg";
+import pic4 from "../../assets/image/picture4.svg";
 import FieldInput from "../../components/common/FieldInput";
 
-const ForgetPassword = () => {
+const VerificationCode = () => {
   const validation = yup.object().shape({
     email: yup.string().required("این فیلد الزامیست!"),
   });
@@ -14,14 +14,13 @@ const ForgetPassword = () => {
     <div className="bg-lightPink min-h-[100vh]">
       <div className="md:flex container block">
         <div className="auth-image-side">
-          <img src={pic3} className="lg:w-2/3 w-[250px]" alt="" />
+          <img src={pic4} className="lg:w-2/3 w-[250px]" alt="" />
         </div>
         <div className="w-full lg:w-5/12 flex justify-center">
           <div className="form-section-auth" dir="rtl">
-            <h2 className="auth-title">فراموشی رمز</h2>
+            <h2 className="auth-title"> کد را وارد کنید</h2>
             <p className="w-[300px] mx-auto pr-[10px] text-base text-right lg:mt-12 mt-10 mb-10 text-lightblue ">
-              در صورتی که رمز عبور خود را فراموش کرده اید، لطفا ایمیل یا موبایل
-              خود را وارد کنید{" "}
+              لطفا کد ارسال شده برای شما را وارد کنید{" "}
             </p>
             <Formik
               initialValues={{ email: "" }}
@@ -30,10 +29,10 @@ const ForgetPassword = () => {
             >
               <Form className="auth-form">
                 <FieldInput
-                  placeholder={"ایمیل را وارد کنید"}
+                  placeholder={"رمز را وارد کنید"}
                   name="email"
-                  icon={"telephone-fill"}
-                  classIcon={"rotate-[260deg] mr-2"}
+                  icon={"bi bi-envelope-arrow-down-fill"}
+                  classIcon={"mr-1"}
                 />
                 <input
                   type="submit"
@@ -49,4 +48,4 @@ const ForgetPassword = () => {
   );
 };
 
-export default ForgetPassword;
+export default VerificationCode;
