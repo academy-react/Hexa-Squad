699cf85915a7e5660dfa8932a44497afc613c5a0
bita <bitaaghanbarii@gmail.com>
2023-10-08 22:42:14 +0330

    background image added to Teachers section

--- src/screens/Landing/landing-components/OurTeachers.jsx
+++ src/screens/Landing/landing-components/OurTeachers.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import background from "../../../assets/image/bgDesign.svg";
 import TeacherCard from "./TeacherCard";
 
 const OurTeachers = () => {
@@ -22,26 +22,32 @@ const OurTeachers = () => {
   ]);
   return (
     <>
-      <h1 className="title1 lg:mt-48 mt-28 mr-10  lg:m-auto">
-        اساتید برتر
-        <span className="absolute w-[100%] block mt-3 h-2 my-1 bg-gradient-to-l from-newPurpleAlpha2 to-transparentPurple2"></span>
-      </h1>
+      <div className=" w-[540px] md:w-[740px] relative mt-20 lg:m-auto  mx-auto lg:mt-48">
+        <h1 className="title1 lg:mt-48 mt-28 lg:m-auto">
+          اساتید برتر
+          <span className="absolute w-[100%] block mt-3 h-2 my-1 bg-gradient-to-l from-newPurpleAlpha2 to-transparentPurple2"></span>
+        </h1>
+      </div>
       <div className="flex items-center mt-20 lg:mt-20 justify-center  container mx-auto ">
-      <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 md:gap-8">
-        {teacherCard.map((item, index) => {
-          return (
-            <>
-              <TeacherCard
-                key={index}
-                title={item.title}
-                description={item.description}
-                course={item.course}
-              />
-            </>
-          );
-        })}
-     </div>
-     </div>
+        <div
+          className=" bg-center bg-cover w-[300px] h-[420px] absolute left-0 mt-44 hidden lg:block "
+          style={{ backgroundImage: `url(${background})` }}
+        ></div>
+        <div className="grid grid-cols-1  md:grid-cols-3  md:gap-4 lg:gap-8">
+          {teacherCard.map((item, index) => {
+            return (
+              <>
+                <TeacherCard
+                  key={index}
+                  title={item.title}
+                  description={item.description}
+                  course={item.course}
+                />
+              </>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 };
--- src/screens/Landing/landing-components/TeacherCard.jsx
+++ src/screens/Landing/landing-components/TeacherCard.jsx
@@ -4,25 +4,25 @@ import teacher from "../../../assets/image/teacher1.jpg";
 const TeacherCard = ({ title, description, course }) => {
   return (
     
-      <div className="relative w-[340px] h-96  ">
+      <div className="relative gap-12 w-[340px] h-96 md:w-[300px] md:h-96 lg:w-[340px] lg:h-96">
         <div className="teacher-photo">
-          <div className="rounded-full absolute shadow-shadow-auth overflow-hidden w-32 h-32 mx-4 my-4 ">
+          <div className="rounded-full absolute shadow-shadow-auth overflow-hidden w-32 h-32  mx-4 my-4  md:w-[88px] md:h-[88px] md:mx-3 md:my-3 lg:w-32 lg:h-32  lg:mx-4 lg:my-4 ">
             <img className="object-cover " src={teacher} alt="image" />
           </div>
         </div>
-        <div className="rounded-2xl cursor-pointer shadow-2xl group overflow-hidden shadow-shadow-Course-box hover:shadow-shadow-auth z-0 mt-[100px] bg-semiWhite ">
-          <div className="p-5 flex-col  relative">
+        <div className="rounded-2xl cursor-pointer mx-auto  shadow-2xl group overflow-hidden shadow-shadow-Course-box hover:shadow-shadow-auth z-0 mt-[100px] lg:mt-[100px] md:mt-[90px] bg-semiWhite md:w-60 lg:w-full ">
+          <div className="p-5 flex-col relative">
             <div className="contact-teachers1 absolute z-3 top-[-60%]">
-              <div className=" bi bi-instagram icons-contact md:mx-4 md:text-base mt-3 "></div>
-              <div className=" bi bi-telegram icons-contact md:mx-4 md:text-base"></div>
-              <div className=" bbi bi-linkedin icons-contact md:mx-4 md:text-base"></div>
+              <div className=" bi bi-instagram icons-contact mt-3  "></div>
+              <div className=" bi bi-telegram icons-contact"></div>
+              <div className=" bbi bi-linkedin icons-contact"></div>
             </div>
 
-            <h2 className="text-xl md:text-2xl mt-20 group-hover:text-[#7400B2] text-darkblue2 ">
+            <h2 className="text-xl md:text-lg lg:text-2xl  mt-20 lg:mt-20 md:mt-12 group-hover:text-[#7400B2] text-darkblue2 ">
               {title}
             </h2>
             <h2 className="text-base text-[#6B3FEA] group-hover:text-[#0017C1] "> {course} </h2>
-            <p className=" text-sm text-[#4E40C4] group-hover:text-[#15006A] opacity-80 w-72 my-4 ">
+            <p className=" text-sm lg:text-sm text-[#4E40C4] group-hover:text-[#15006A] opacity-80 w-72 my-4 md:mt-2 md:text-xs md:w-56 lg:w-full ">
               {description}
             </p>
           </div>
