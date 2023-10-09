baffdb1c8706c6633f44cb6ca693c4f842612908
bita <bitaaghanbarii@gmail.com>
2023-10-09 19:26:12 +0330

    Updates on darkMode

--- src/screens/Landing/landing-components/Suggestions.jsx
+++ src/screens/Landing/landing-components/Suggestions.jsx
@@ -9,16 +9,20 @@ const Suggestions = () => {
     massage: yup.string().required("این فیلد الزامیست!"),
   });
   return (
-    <div className=" mt-[-30px]">
-      <h1 className="title">
-        انتقادات و پیشنهادات
-        <span className="absolute w-[100%]  block mt-3 h-2 my-1 bg-gradient-to-l from-newPurpleAlpha2 to-transparentPurple2"></span>
-      </h1>
-      <div className="flex 	justify-center mb-56 content-center items-stretch ">
+    <>
+      <div className=" w-[540px] md:w-[740px] relative mt-20 lg:m-auto  mx-auto lg:mt-48">
+        <h1 className=" relative title1 lg:mt-52 mt-20 md:mt-36 mx-auto">
+          انتقادات و پیشنهادات
+          <span className="absolute w-[100%]  block mt-3 h-2 my-1 bg-gradient-to-l from-newPurpleAlpha2 to-transparentPurple2"></span>
+        </h1>
+      </div>
+      <div className="flex 	justify-center mb-56 content-center items-stretch lg:mt-10">
         <div
-          className=" flex w-[460px] relative self-center bg-semiWhite dark:bg-newPurpleAlpha2 lg:w-[600px] min-h-[400px] max-h-[530px] py-8
-      mt-28 lg:flex rounded-lg  lg:shadow-shadow-auth shadow-shadow-transparent  flex-col "
-        >
+          className=" bg-center bg-cover w-[400px] h-[460px] absolute right-0 mt-20 hidden lg:block"
+          style={{ backgroundImage: `url(${background})` }}
+        ></div>
+
+        <div className="suggestions-box ">
           <div dir="rtl">
             <Formik
               initialValues={{ email: "", massage: "" }}
@@ -27,13 +31,13 @@ const Suggestions = () => {
             >
               <Form className="text-lightblue text-right  px-10 flex-col items-center flex">
                 <Field
-                  className="field-input2 relative w-full my-2 p-3 dark:bg-transparentPurple"
+                  className="field-input2 relative w-full my-2 p-3"
                   placeholder={"ایمیل را وارد کنید"}
                   name="email"
                 />
 
                 <textarea
-                  className="relative w-full h-[300px] field-input2 dark:bg-transparentPurple"
+                  className="relative w-full h-[300px] field-input2"
                   placeholder="متن را وارد کنید"
                   name="obs"
                 ></textarea>
@@ -41,14 +45,14 @@ const Suggestions = () => {
                 <input
                   type="submit"
                   value=" ارسال پیام"
-                  className="primary-button w-full p-4"
+                  className="primary-btn text-semiPink rounded-lg mt-2 w-full p-4"
                 />
               </Form>
             </Formik>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
