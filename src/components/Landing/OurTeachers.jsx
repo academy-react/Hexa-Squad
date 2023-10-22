5dd3657f9bbace5f644a7d7ec91a01b518acfdd0
bita <bitaaghanbarii@gmail.com>
2023-10-22 21:14:45 +0330

    some changes on OurTeachers sections

--- src/components/Landing/OurTeachers.jsx
+++ src/components/Landing/OurTeachers.jsx
@@ -9,22 +9,19 @@ const OurTeachers = () => {
       title: "نام استاد شماره 1",
       course: " مدرس ری اکت",
       description: `”موفقیت نهایی نیست. شکست کشنده نیست. این شجاعت ادامه دادن است که مهم است.“`,
-      imageUrl:
-        "https://toplearn.com/img/user/teachers/9420_1b6e5884-854f-961b-725b-39f3fa1ee56a_%D9%85%D8%AC%D8%AA%D8%A8%DB%8C_%DA%A9%D8%B1%D9%85%D9%84%D9%88.jpg",
+     
     },
     {
       title: "نام استاد شماره 2",
       course: " مدرس جاوا",
       description: `”اهمیتی نداره تو چکار میکنی؛ راکستار آن باشید. اگر به همین راحتی بود، همه این کار را می کردند.“`,
-      imageUrl:
-        "https://toplearn.com/img/user/teachers/user-profile-%DB%8C%DA%A9-%D8%B4%D9%86%D8%A8%D9%87-%DB%B7-%D8%A7%D8%B1%D8%AF%DB%8C%D8%A8%D9%87%D8%B4%D8%AA-%DB%B1%DB%B3%DB%B9%DB%B9-98501911-414.jpg",
+      
     },
     {
       title: "نام استاد شماره 3",
       course: " مدرس انگولار",
       description: `”مثبت اندیشی فقط امید به بهترین ها نیست! بلکه بهتر است بهترین عملکرد خود را در عمل به کار ببندید.“`,
-      imageUrl:
-        "https://toplearn.com/img/user/teachers/fe05078d-ff6b-416d-bfac-5b10af7c54ef_software-developer-gd82689c10_1920.jpg",
+      
     },
   ]);
   const mapTeacherCard = teacherCard.map((item, index) => {
@@ -34,7 +31,7 @@ const OurTeachers = () => {
         title={item.title}
         description={item.description}
         course={item.course}
-        image={item.imageUrl}
+       
       />
     );
   });
@@ -48,7 +45,7 @@ const OurTeachers = () => {
       </div>
       <div className="flex items-center mt-20 lg:mt-20 justify-center  container mx-auto ">
         <div
-          className=" bg-center bg-cover w-[300px] h-[420px] absolute left-0 mt-44 hidden lg:block "
+          className=" bg-center bg-cover w-[210px] h-[300px] absolute left-0 mt-[340px] hidden lg:block "
           style={{ backgroundImage: `url(${background})` }}
         ></div>
         <div className="grid grid-cols-1  md:grid-cols-3  md:gap-4 lg:gap-8">
@@ -59,4 +56,5 @@ const OurTeachers = () => {
   );
 };
 
+// w-[300px] h-[420px]
 export default OurTeachers;
--- src/components/Landing/TeacherCard.jsx
+++ src/components/Landing/TeacherCard.jsx
@@ -1,36 +1,35 @@
 import React from "react";
+import teacher from "../../assets/image/teacher1.jpg";
 
-
-const TeacherCard = ({ image,title, description, course }) => {
+const TeacherCard = ({ title, description, course }) => {
   return (
-    
-      <div className="relative gap-12 w-[340px] h-96 md:w-[300px] md:h-96 lg:w-[340px] lg:h-96">
-        <div className="teacher-photo dark:bg-[#2a1079] dark:shadow-[#351476]">
-          <div className="rounded-full absolute shadow-shadow-auth  overflow-hidden w-32 h-32  mx-2 my-2  md:w-[96px] md:h-[96px] md:mx-2 md:my-2 lg:w-32 lg:h-32  lg:mx-2 lg:my-2 ">
-            <img className="object-cover shadow-shadow-auth " src={image} alt="image" />
-          </div>
-        </div>
-        <div className="rounded-2xl  cursor-pointer mx-auto  shadow-2xl group overflow-hidden shadow-shadow-Course-box hover:shadow-shadow-auth  z-0 mt-[100px] lg:mt-[100px] md:mt-[90px] bg-semiWhite dark:bg-[#4116afe5] opacity md:w-60 lg:w-full dark:hover:shadow-[#220e6b] ">
-          <div className="p-5 flex-col relative h-[260px] lg:h-[260px] md:h-[200px]">
-            <div className="contact-teachers1 absolute z-3 top-[-60%] dark:bg-[#2a1079]">
-              <div className=" bi bi-instagram icons-contact mt-3  "></div>
-              <div className=" bi bi-telegram icons-contact"></div>
-              <div className=" bbi bi-linkedin icons-contact"></div>
-            </div>
-
-            <h2 className="text-xl md:text-lg lg:text-2xl  mt-20 lg:mt-20 md:mt-12 group-hover:text-[#7400B2] text-darkblue2 dark:text-[#c9baff] dark:group-hover:text-[#e4defa]">
-              {title}
-            </h2>
-            <h2 className="text-base text-[#6B3FEA] dark:text-[#7554F2] group-hover:text-[#0017C1] dark:group-hover:text-[#AC7CEB]"> {course} </h2>
-            <p className=" text-sm md:text-xs  lg:text-sm text-[#1a1920] dark:text-[#8C7BFF] dark:group-hover:text-[#BAAFFF] group-hover:text-[#15006A] opacity-80 w-72 my-4 mt-4  md:w-48 lg:w-full ">
-              {description}
-            </p>
-          </div>
+    <div className="relative gap-12 w-[340px] h-96 md:w-[300px] md:h-96 lg:w-[340px] lg:h-96">
+      <div className="teacher-photo dark:bg-[#2a1079] dark:shadow-[#351476]">
+        <div className="rounded-full absolute overflow-hidden shadow-[0_20px_50px_rgba(88,_0,_255,_1)]  w-32 h-32  mx-2 my-2  md:w-[96px] md:h-[96px] md:mx-2 md:my-2 lg:w-32 lg:h-32 lg:mx-2 lg:my-2 ">
+          <img className="object-cover " src={teacher} alt="image" />
         </div>
       </div>
-  
-  
-    
+      <div className="rounded-br-3xl rounded-xl  cursor-pointer mx-auto  shadow-[rgba(0,_0,_0,_0.15)_0px_25px_50px_-12px] group overflow-hidden  hover:shadow-shadow-auth  z-0 mt-[100px] lg:mt-[100px] md:mt-[90px]  bg-gradient-to-bl from-[#5800FF] to-[#6363ff] dark:bg-[#4116afe5] opacity md:w-60 lg:w-full dark:hover:shadow-[#220e6b] ">
+        <div className="p-5 flex-col relative h-[260px] lg:h-[260px] md:h-[200px]">
+          <div className="contact-teachers1 absolute z-3 top-[-60%] dark:bg-[#2a1079]">
+            <div className=" bi bi-instagram icons-contact mt-3  "></div>
+            <div className=" bi bi-telegram icons-contact"></div>
+            <div className=" bbi bi-linkedin icons-contact"></div>
+          </div>
+
+          <h2 className="text-xl md:text-lg lg:text-2xl  mt-20 lg:mt-20 md:mt-12 text-[#ebe5ff]   dark:text-[#c9baff] dark:group-hover:text-[#e4defa]">
+            {title}
+          </h2>
+          <h2 className="text-sm mt-2 text-[#baa2ff] dark:text-[#a384ff]  dark:group-hover:text-[#AC7CEB]">
+            {" "}
+            {course}{" "}
+          </h2>
+          <p className=" text-sm md:text-xs  lg:text-sm text-[#ccc1fd] dark:text-[#8C7BFF] dark:group-hover:text-[#BAAFFF]   w-72 my-4 mt-4  md:w-48 lg:w-full ">
+            {description}
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
