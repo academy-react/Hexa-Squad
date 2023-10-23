b773e31cd84783f7dc42352b8ff1dd14a16363ed
shamim_hb <shamim.bagheri31@gmail.com>
2023-10-23 14:38:47 +0330

    Landing/Course Slider

--- src/components/Landing/CourseEducation.jsx
+++ src/components/Landing/CourseEducation.jsx
@@ -2,6 +2,7 @@ import React, { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
 import Title from "../common/Title";
 import Course from "../course/Course";
+import CourseAutoplaySlider from "../course/CourseAutoplaySlider";
 import CourseSlider from "../course/CourseSlider";
 import WhishList from "../../core/services/api/Data/WhishList";
 
@@ -9,27 +10,27 @@ import "./common.css";
 import "../../index.css";
 
 const CourseEducation = () => {
-  const [coursesList, setCoursesList] = useState(WhishList);
-  const mapCard = coursesList.map((item, index) => {
-    return (
-      <Course
-        key={index}
-        title={item.title}
-        courseCount={item.courseCount}
-        time={item.time}
-        date={item.date}
-        professorName={item.professorName}
-        like={item.like}
-        width={"lg:w-[24%]"}
-        dislike={item.dislike}
-        studentCount={item.studentCount}
-        price={item.price}
-        image={item.imageUrl}
-      />
-    );
-  });
+  // const [coursesList, setCoursesList] = useState(WhishList);
+  // const mapCard = coursesList.map((item, index) => {
+  //   return (
+  //     <Course
+  //       key={index}
+  //       title={item.title}
+  //       courseCount={item.courseCount}
+  //       time={item.time}
+  //       date={item.date}
+  //       professorName={item.professorName}
+  //       like={item.like}
+  //       width={"lg:w-[24%]"}
+  //       dislike={item.dislike}
+  //       studentCount={item.studentCount}
+  //       price={item.price}
+  //       image={item.imageUrl}
+  //     />
+  //   );
+  // });
   return (
-    <div className="lg:max-w-[1340px] md:max-w-[750px] sm:min-w-[640px] mx-auto mt-[-30px]">
+    <div className="lg:max-w-[1400px] md:max-w-[750px] sm:min-w-[640px] mx-auto mt-[-30px]">
       <div className="course">
         <Title
           className={"lg:mt-40 mt-20 lg:block"}
@@ -41,9 +42,12 @@ const CourseEducation = () => {
           </button>
         </Link>
       </div>
-      <div className="mt-24 md:flex gap-4 lg:gap-3 lg:flex-row md:flex-wrap hidden">
-        {mapCard}
+      <div className="mt-24 md:block gap-4 lg:gap-3 hidden">
+        <CourseAutoplaySlider/>
       </div>
+      {/* <div className="mt-24 md:flex gap-4 lg:gap-3 lg:flex-row md:flex-wrap hidden">
+        {mapCard}
+      </div> */}
       <div className="mt-20 md:hidden">
         <CourseSlider />
       </div>
