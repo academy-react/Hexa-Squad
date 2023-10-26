0434c590d58c6e8c1b6b41cb3076502e62ae3ba5
shamim_hb <shamim.bagheri31@gmail.com>
2023-10-26 19:10:29 +0330

    Scroll To Top

--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -1,11 +1,13 @@
-import React, { Fragment } from "react";
+import React from "react";
 import Header from "../Header/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
+import ScrollToTop from "../common/ScrollToTop";
 
 const Main = () => {
   return (
     <div className="bg-lightPink dark:bg-darkblue min-h-[100vh]"dir="rtl" >
+      <ScrollToTop/>
       <Header />
       <Outlet />
       <Footer/>
