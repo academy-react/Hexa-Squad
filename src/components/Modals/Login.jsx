8a3c57a6ca60d3d2307d15e8f677594e64cf399c
shamim_hb <shamim.bagheri31@gmail.com>
2023-10-30 23:46:03 +0330

    UserPanel/DarkMode

--- src/components/Modals/Login.jsx
+++ src/components/Modals/Login.jsx
@@ -6,17 +6,17 @@ import {validation} from '../../core/validations/validations';
 
 const Login = () => {
   return (
-    <div className="form-section-auth loginModal fixed w-[400px] pt-12 top-[16%] lg:left-[38%] md:left-[22%] sm:left-[10%] left-[5%] right-[50%] z-[500] shadow-shadow-all hidden bg-whitePink" dir='rtl'>
-      <label htmlFor="showLoginModal" className='cursor-pointer'><i className='absolute bi bi-x top-5 right-5 text-newPurpleAlpha2 text-4xl'></i></label>
+    <div className="form-section-auth loginModal fixed w-[400px] pt-12 top-[16%] lg:left-[38%] md:left-[22%] sm:left-[10%] left-[5%] right-[50%] z-[500] shadow-shadow-all hidden bg-lightPink" dir='rtl'>
+      <label htmlFor="showLoginModal" className='cursor-pointer'><i className='absolute bi bi-x top-5 right-5 text-violet-800 text-4xl'></i></label>
       
-        <h2 className='auth-title mt-0 text-lightblue ' >ورود به سایت</h2>
+        <h2 className='auth-title mt-0 text-violet-800 ' >ورود به سایت</h2>
         <Formik initialValues={{email:'',password:''}} validationSchema={validation} onSubmit={()=>alert('ورود به سایت')} >
             <Form className="auth-form">
                 <FieldInput  placeholder={"ایمیل را وارد کنید"} name="email" icon={"telephone-fill"} classIcon={'rotate-[260deg]'} />
                 <FieldInput  placeholder={"رمز عبور را وارد کنید"} name="password" icon={"lock-fill"} type="password"  />
-                <Link to={"/authorize/Verification"} className="mb-12 ml-[-200px] pointer">فراموشی رمز؟</Link>
+                <Link to={"/authorize/Verification"} className="mb-12 ml-[-200px] pointer text-[#7F52FD]">فراموشی رمز؟</Link>
                 <input type="submit" value="ورود" className="primary-button w-full p-4"/>
-                <label className="mt-4" > قبلا ثبت نام نکرده‌اید؟ <Link to={"/authorize/register"} className="pointer underline">ثبت نام</Link></label>                            
+                <label className="my-4" > قبلا ثبت نام نکرده‌اید؟ <Link to={"/authorize/register"} className="pointer underline">ثبت نام</Link></label>                            
             </Form>
         </Formik>
     </div>
