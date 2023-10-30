f789dbbde84896d83db23cf26e7117e216689c35
bita <bitaaghanbarii@gmail.com>
2023-10-31 01:05:04 +0330

    some additional changes for News Card

--- src/components/Landing/News.jsx
+++ src/components/Landing/News.jsx
@@ -9,12 +9,17 @@ const News = () => {
       img: "https://toplearn.com/img/post/%DA%86%D8%B1%D8%A7_%DB%8C%DA%A9_%D8%B3%D8%A6%D9%88_%DA%A9%D8%A7%D8%B1_%D8%A8%D8%A7%DB%8C%D8%AF_%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D9%87_%D9%86%D9%88%DB%8C%D8%B3_%D8%AE%D9%88%D8%A8%DB%8C_%D8%A8%D8%A7%D8%B4%D8%AF%D8%9F.jpg",
       description:
         " برای ساخت وب سایت شما می توانید از روش های گوناگونی چون وردپرس و .... استفاده کنید .  ",
+
+      views: "123",
+      date: "1402/02/08",
     },
     {
       name: "8 دلیل انتخاب تایپ اسکریپت در سال 2022",
       img: "https://toplearn.com/img/post/google_colab_%DA%86%DB%8C%D8%B3%D8%AA%D8%9F.jpg",
       description:
-        " محبوبیت جاوااسکریپت در میان توسعه دهندگان فرانت اند و بک اند کارآمدترین یا مقرون به صرفه ترین راه‌ حل نیست. ",
+        "  جاوااسکریپت در میان توسعه دهندگان فرانت اند و بک اند کارآمدترین یا مقرون به صرفه ترین ... ",
+      views: "173",
+      date: "1401/03/09",
     },
   ]);
   const newsCardsMapper = newsList.map((item, index) => {
@@ -23,18 +28,20 @@ const News = () => {
         img={item.img}
         name={item.name}
         description={item.description}
+        views={item.views}
+        date={item.date}
         key={index}
       />
     );
   });
   return (
-    <section className="mb-28"> 
+    <section className="mb-28">
       <div className=" w-[440px] md:w-[740px] relative mt-20 lg:m-auto  mx-auto lg:mt-56">
         <Title inner={"اخبار و مقالات"} className={"title1"} />
         <Link to="/news" className={"text-newWhite hover:text-newWhite"}>
-        <button className="absolute primary-btn w-auto h-10 bottom-[-20px] lg:w-56 lg:my-10  py-1 md:block lg:hidden left-3">
-          مشاهده ی همه
-        </button>
+          <button className="absolute primary-btn w-auto h-10 bottom-[-20px] lg:w-56 lg:my-10  py-1 md:block lg:hidden left-3">
+            مشاهده ی همه
+          </button>
         </Link>
       </div>
 
--- src/components/Landing/TeacherCard.jsx
+++ src/components/Landing/TeacherCard.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
+import teacher1 from "../../assets/image/drBahr.jpg"
 
 import teacher from "../../assets/image/teacher1.jpg";
 
@@ -14,7 +15,7 @@ const TeacherCard = ({ title, description, course }) => {
       >
         <div className="teacher-photo dark:bg-[#2a1079] dark:shadow-[#351476] ">
           <div className="rounded-full absolute overflow-hidden shadow-[0_20px_50px_rgba(88,_0,_255,_1)] dark:shadow-shadow-auth w-32 h-32  mx-2 my-2  md:w-[96px] md:h-[96px] md:mx-2 md:my-2 lg:w-32 lg:h-32 lg:mx-2 lg:my-2 ">
-            <img className="object-cover " src={teacher} alt="image" />
+            <img className="object-cover " src={teacher1} alt="image" />
           </div>
         </div>
         <div className="rounded-br-3xl rounded-lg cursor-pointer mx-auto  shadow-[rgba(0,_0,_0,_0.15)_0px_25px_50px_-12px] group overflow-hidden  hover:shadow-shadow-auth  z-0 mt-[100px] lg:mt-[100px] md:mt-[90px]  bg-gradient-to-bl from-[#8800ff] to-[#8181ff] dark:bg-gradient-to-bl dark:from-[#13005A] dark:to-[#26126e]  opacity md:w-60 lg:w-full dark:hover:shadow-[#220e6b] ">
@@ -25,12 +26,12 @@ const TeacherCard = ({ title, description, course }) => {
               <div className=" bbi bi-linkedin icons-contact"></div>
             </div>
             <Link to="/TeacherProfile">
-              <h2 className="text-xl md:text-lg lg:text-2xl  mt-20 lg:mt-20 md:mt-12 text-[#ede8ff]   dark:text-[#c9baff] dark:group-hover:text-[#e4defa]">
+              <h2 className="text-xl md:text-lg lg:text-xl  mt-20 lg:mt-20 md:mt-12 text-[#ede8ff]   dark:text-[#c9baff] dark:group-hover:text-[#e4defa]">
                 {title}
               </h2>
             </Link>
             <h2 className="text-sm mt-2 text-[#d5c7ff]"> {course} </h2>
-            <p className=" text-sm md:text-xs  lg:text-sm text-[#ccc1fd]   w-72 my-4 mt-4  md:w-48 lg:w-full ">
+            <p className=" text-sm md:text-xs  lg:text-sm text-indigo-100 lg:leading-6  w-72 my-4 mt-4  md:w-48 lg:w-full ">
               {description}
             </p>
           </div>
